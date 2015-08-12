a89b8745fdad8953b6027ab9640f2e772de44699
Travis Johnston <tjohnst1@gmail.com>
2015-08-12 09:33:16 -0700

    create model and year tests

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -13,4 +13,5 @@ class Vehicle
 
 
 
+
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,4 +8,17 @@ describe('Vehicle') do
       expect(new_vehicle.make).to(eq('Toyota'))
     end
   end
+  describe('#model') do
+    it('returns the model of the vehicle') do
+      new_vehicle = Vehicle.new('Toyota', 'Prius', '2011')
+      expect(new_vehicle.model).to(eq('Prius'))
+    end
+  end
+  describe('#year') do
+    it('returns the year of the vehicle') do
+      new_vehicle = Vehicle.new('Toyota', 'Prius', '2011')
+      expect(new_vehicle.year).to(eq('2011'))
+    end
+  end
+
 end
