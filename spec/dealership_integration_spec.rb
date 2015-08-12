46506814b44e4695d696d8f95fd0949e59ddcb1d
Kellen Kolbeck & Travis Johnston <tjohnst1@gmail.com>
2015-08-12 11:54:19 -0700

    Add integration spec.

---
+++ spec/dealership_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+require('pry')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('shows the homepage where dealerships are listed', {:type => :feature}) do
+  it('allows user to input car dealerships') do
+    visit('/')
+    fill_in('dealership_name', :with => "Bob's Autos")
+    click_button('Submit')
+    expect(page).to have_content("Bob's Autos")
+  end
+end    
