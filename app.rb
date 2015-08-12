78f193222c66934c64531b1ce656b87756187b69
Kellen Kolbeck & Travis Johnston <tjohnst1@gmail.com>
2015-08-12 15:15:15 -0700

    Add vehicles page routing to display a newly inputed vehicle.

--- app.rb
+++ app.rb
@@ -9,13 +9,30 @@ get('/') do
   erb(:index)
 end
 
+get('/dealerships') do
+  @dealerships = Dealership.all()
+end
+
 post('/dealerships') do
   new_dealership = Dealership.new(params.fetch('dealership_name'))
-  new_dealership.save
+  new_dealership.save()
+  @dealerships = Dealership.all()
   erb(:success)
 end
 
 get('/dealership/:id') do
   @dealership = Dealership.find(params.fetch('id').to_i())
   erb(:dealership)
+end
+
+post('/vehicles') do
+
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  @new_vehicle = Vehicle.new(make, model, year)
+  @new_vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.save_vehicle(@new_vehicle)
+  erb(:success)
 end
