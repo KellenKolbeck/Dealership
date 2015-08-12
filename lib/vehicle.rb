d1c39124d18f7d191f293ffbb555b24708f684d9
Travis Johnston <tjohnst1@gmail.com>
2015-08-12 09:28:12 -0700

    create make method for vehicle class

---
+++ lib/vehicle.rb
@@ -0,0 +1,16 @@
+class Vehicle
+
+  define_method(:initialize) do |make, model, year|
+    @make = make
+    @model = model
+    @year = year
+  end
+
+  define_method(:make) do
+    @make
+  end
+
+
+
+
+end
