697e1bf0d9a1cdc7cbd3176c19bda263c52acaf5
Kellen Kolbeck & Travis Johnston <tjohnst1@gmail.com>
2015-08-12 14:19:37 -0700

    add clear to run before each test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,7 +3,9 @@ require('vehicle')
 require('dealership')
 
 describe('Dealership') do
-
+  before() do
+    Dealership.clear
+  end  
   describe('#name') do
     it('returns the name of the dealership') do
       test_dealership = Dealership.new('Bob Auto')
